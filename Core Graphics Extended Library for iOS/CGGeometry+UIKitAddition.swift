9f6503891f6f25df0f74191d75b8d2371b877817
WeZZard <wezzardlau@gmail.com>
2015-06-29 14:25:17 +0800

    Removes default cases for complete evaluated enums

--- Core Graphics Extended Library for iOS/CGGeometry+UIKitAddition.swift
+++ Core Graphics Extended Library for iOS/CGGeometry+UIKitAddition.swift
@@ -20,8 +20,6 @@ extension CGFloat {
             return ceil(self * screenScale) / screenScale
         case .Floor:
             return ceil(self * screenScale) / screenScale
-        default:
-            return self
         }
     }
 }
@@ -34,8 +32,6 @@ extension CGPoint {
             return CGPoint(x: ceil(self.x * screenScale) / screenScale, y: ceil(self.y * screenScale) / screenScale)
         case .Floor:
             return CGPoint(x: floor(self.x * screenScale) / screenScale, y: floor(self.y * screenScale) / screenScale)
-        default:
-            return self
         }
     }
 }
@@ -48,8 +44,6 @@ extension CGSize {
             return CGSize(width: ceil(self.width * screenScale) / screenScale, height: ceil(self.height * screenScale) / screenScale)
         case .Floor:
             return CGSize(width: floor(self.width * screenScale) / screenScale, height: floor(self.height * screenScale) / screenScale)
-        default:
-            return self
         }
     }
 }
@@ -68,8 +62,6 @@ extension CGRect {
                 y: floor(self.origin.y * screenScale) / screenScale,
                 width: floor(self.size.width * screenScale) / screenScale,
                 height: floor(self.size.height * screenScale) / screenScale)
-        default:
-            return self
         }
     }
 }
